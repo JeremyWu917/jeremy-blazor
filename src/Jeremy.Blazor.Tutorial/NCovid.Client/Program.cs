541eada0e24d6c4cb2937278f870f0f4a30d2393
JeremyWu917 <1280442096@qq.com>
2022-09-12 15:20:38 +0800

    Complete IHttpClientFactory sample

--- src/Jeremy.Blazor.Tutorial/NCovid.Client/Program.cs
+++ src/Jeremy.Blazor.Tutorial/NCovid.Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NCovid.Client;
+using NCovid.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,6 +9,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:9000") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:9000") });
+
+builder.Services.AddHttpClient<IEmployeeService, EmployeeService>(client => client.BaseAddress = new Uri("http://localhost:9000"));
 
 await builder.Build().RunAsync();
---
+++ src/Jeremy.Blazor.Tutorial/NCovid.Client/Services/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using NCovid.Shared.Dtos;
+
+namespace NCovid.Client.Services
+{
+    public interface IEmployeeService
+    {
+        Task<IEnumerable<EmployeeDto>> GetForDepartmentAsync(int departmentId);
+        Task<EmployeeDto> GetOneForDepartmentAsync(int departmentId, int id);
+        Task<EmployeeDto> AddForDepartmentAsync(int departmentId, EmployeeAddOrUpdateDto employee);
+        Task UpdateForDepartmentAsync(int departmentId, int id, EmployeeAddOrUpdateDto employee);
+        Task DeleteFromDepartmentAsync(int departmentId, int id);
+    }
+}
